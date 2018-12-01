8abd81f6982b10de2c1ddc7adb4aca9f776bbeb6
Derrick Weis <derrick@derrickweis.com>
2018-11-30 21:31:01 -0500

    fix brightness controls

--- hosts/monoid/configuration.nix
+++ hosts/monoid/configuration.nix
@@ -38,8 +38,8 @@
   services.actkbd = {
     enable = true;
     bindings = [
-      { keys = [ 224 ]; events = [ "key" ]; command = "/run/wrappers/bin/light -U 10"; }
-      { keys = [ 225 ]; events = [ "key" ]; command = "/run/wrappers/bin/light -A 10"; }
+      { keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 10"; }
+      { keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 10"; }
     ];
   };
 
