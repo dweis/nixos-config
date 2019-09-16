60ce1c49877f699b21409dda7c53b4fa90858c53
Derrick Weis <derrick@derrickweis.com>
2019-09-15 21:54:02 -0400

    Fix brightness keys

--- host-monoid.nix
+++ host-monoid.nix
@@ -56,6 +56,15 @@
   hardware.opengl.driSupport32Bit = true;
   hardware.bluetooth.enable = true;
 
+  programs.light.enable = true;
+  services.actkbd = {
+    enable = true;
+    bindings = [
+      { keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 5"; }
+      { keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 5"; }
+    ];
+  };
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
